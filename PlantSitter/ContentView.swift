afbfd02d92bb052f8c0f6e53169a23d3a371ec5a
Kal <kal.aalrajhi@gmail.com>
2023-07-03 10:50:55 -0400

    Initial Commit

---
+++ PlantSitter/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  PlantSitter
+//
+//  Created by Dr Cpt Blackbeard on 7/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            ScrollView(.horizontal) {
+//                HStack(spacing: 10) {
+//                    ForEach(0..<10) {
+//                        Text("Item \($0)")
+//                            .font(.largeTitle)
+//                            .foregroundColor(.white)
+//                            .frame(width: 150, height: 150)
+//                            .background(.green)
+//                    }
+//                }
+//            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlantSitter/PlantSitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantSitterApp.swift
+//  PlantSitter
+//
+//  Created by Dr Cpt Blackbeard on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantSitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
