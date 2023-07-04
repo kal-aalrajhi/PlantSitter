d61a055466e82a85abdfaa5d45399d6d78f3a6d2
Kal <kal.aalrajhi@gmail.com>
2023-07-04 10:44:03 -0400

    Add 'plus' button without functionality

--- PlantSitter/ContentView.swift
+++ PlantSitter/ContentView.swift
@@ -8,38 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var plants = Plants()
+    
     var body: some View {
         NavigationView {
-            VStack {
-                ScrollView(.horizontal) {
-                    HStack(spacing: 10) {
-                        ForEach(0..<10) {
-                            Text("Room \($0)")
-                                .font(.largeTitle)
-                                .foregroundColor(.white)
-                                .frame(width: 150, height: 150)
-                                .background(.green)
+            // Room Scroll
+            ZStack {
+                VStack {
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 10) {
+                            ForEach(0..<10) {
+                                Text("Room \($0)")
+                                    .font(.largeTitle)
+                                    .foregroundColor(.white)
+                                    .frame(width: 150, height: 150)
+                                    .background(.green)
+                            }
+                        }
+                    }
+                    // Plant List
+                    List {
+                        ForEach(plants.plantItems) { plant in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("Plant")
+                                        .font(.headline)
+                                    Text("Plant Type")
+                                        .font(.subheadline)
+                                }
+                                Spacer()
+                                Text("<img>") // add conditional for place holder vs actual image
+                                    .frame(width: 50, height: 50)
+                                    .background(.teal)
+                            }
                         }
                     }
                 }
-                List {
-                    ForEach(0..<20) { plant in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("Plant")
-                                    .font(.headline)
-                                Text("Plant Type")
-                                    .font(.subheadline)
-                            }
-                            Spacer()
-                            Text("<img>") // add conditional for place holder vs actual image
-                                .frame(width: 50, height: 50)
-                                .background(.teal)
+                .navigationTitle("Dashboard")
+                VStack {
+                    Spacer()
+                        Button(action: {
+                            // action when button is tapped
+                            print("Button tapped!")
+                        }) {
+                            Image(systemName: "plus")
+                                .font(.system(size: 30))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(40)
+                                .shadow(color: .gray, radius: 3.0, x: 1.0, y: 1.0)
+                                .padding(.trailing)
                         }
-                    }
+//                        .padding(.bottom)
                 }
             }
-            .navigationTitle("Dashboard")
         }
     }
 }
